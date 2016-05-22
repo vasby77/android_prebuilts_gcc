8cf9a75753a2dc13997fbfcf237c56ba5d9652ac
ZIM555 <zim555@mail.ru>
2016-05-22 15:11:54 +0300

    Add gcc 4.7

---
+++ arm-eabi-4.7/lib/gcc/arm-eabi/4.7.4/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = ".././../gcc/gcc-4.7/configure --prefix=/home/cl3kener/tmp/Google-arm-eabi-4.7 --target=arm-eabi --host=x86_64-unknown-linux-gnu --build=x86_64-unknown-linux-gnu --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --with-gmp=/home/cl3kener/TOOLCHAIN/Google/build/temp-install --with-mpfr=/home/cl3kener/TOOLCHAIN/Google/build/temp-install --with-mpc=/home/cl3kener/TOOLCHAIN/Google/build/temp-install --without-ppl --without-cloog --without-isl --disable-libssp --enable-threads --disable-nls --disable-libmudflap --disable-libgomp --disable-libstdc__-v3 --disable-sjlj-exceptions --disable-shared --disable-tls --disable-libitm --with-float=soft --with-fpu=vfp --with-arch=armv5te --enable-target-optspace --with-abi=aapcs --prefix=/home/cl3kener/tmp/Google-arm-eabi-4.7 --with-local-prefix=/usr --with-gcc-version=4.7 --with-pkgversion=Uber-SaberMod-4.7.4 --with-binutils-version=2.24 --with-cloog-version=0.18.0 --with-isl-version=0.11.0 --with-gmp-version=5.0.5 --with-mpfr-version=3.1.1 --with-mpc-version=1.0.1 --with-gdb-version=7.7 --with-sysroot=/ --program-transform-name='s&^&arm-eabi-&'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "aapcs" }, { "arch", "armv5te" }, { "float", "soft" }, { "fpu", "vfp" } };
